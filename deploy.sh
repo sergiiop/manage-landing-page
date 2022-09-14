3b4ed4f1d4de30ac8012a077d8a23262c7603e0d
Sergio Pérez <sergio-luis98@hotmail.com>
2022-09-14 16:03:59 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<sergiiop>/<DOM-Manipulation>.git main:gh-pages
+# git push -f git@github.com:<sergiiop>/<manage-landing-page>.git main:gh-pages
 
 cd -
